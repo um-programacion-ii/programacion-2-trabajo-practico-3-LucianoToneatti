3284295dd9986982845612b4b8b6c294f0da0407
LucianoToneatti <l.toneatti@alumno.um.edu.ar>
2025-04-29 01:27:21 -0300

    Cree los Test para la clase Prestamo y SistemaPrestamos, simulando el catalogo de libros y probando el flujo prestamo

--- Trabajo-3/src/main/java/org/example/SistemaPrestamos.java
+++ Trabajo-3/src/main/java/org/example/SistemaPrestamos.java
@@ -1,20 +1,35 @@
 package org.example;
 
+import lombok.Getter;
+
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class SistemaPrestamos {
+    @Getter
     private List<Prestamo> prestamos;
+    private final Catalogo catalogo;
 
-    public SistemaPrestamos() {
+    public SistemaPrestamos(Catalogo catalogo) {
         this.prestamos = new ArrayList<>();
+        this.catalogo = catalogo;
     }
 
-    public void registrarPrestamo(Prestamo prestamo) {
-        prestamos.add(prestamo);
+    public Prestamo prestarLibro(String isbn) {
+        Optional<Libro> libroOptional = catalogo.buscarPorIsbn(isbn);
+        if (libroOptional.isEmpty()) {
+            throw new RuntimeException("Libro no encontrado");
+        }
+
+        Libro libro = libroOptional.get();
+        if (libro.getEstado() == Estado.PRESTADO) {
+            throw new RuntimeException("Libro no disponible");
+        }
+        libro.setEstado(Estado.PRESTADO);
+        return new Prestamo(LocalDate.now(), libro);
     }
 
-    public List<Prestamo> getPrestamos() {
-        return prestamos;
-    }
+
 }
